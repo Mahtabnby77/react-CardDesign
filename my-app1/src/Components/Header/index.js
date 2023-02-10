be2e01b91ff6182917ea04eb1703dc92ac8549b2
mahtabnabi98 <mahtabnby777@gmail.com>
2023-02-10 23:25:32 +0330

    message

---
+++ my-app1/src/Components/Header/index.js
@@ -0,0 +1,11 @@
+import  './style.css';
+
+function Header() {
+    return (
+        <header className='header'>
+            <h1>لیست کارها</h1>
+        </header>
+    )
+}
+
+export default Header
